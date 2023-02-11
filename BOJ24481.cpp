318097690cbf237d4659ed5e8a4b38be29454197
taegyun12 <97075534+taegyun12@users.noreply.github.com>
2023-02-11 20:09:08 +0900

    Create BOJ24481.cpp

---
+++ BOJ24481.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+static vector<vector<int>> adj_list;
+static vector<bool> visited;
+int cnt = 1;
+void DFS(int now, int depth);
+static vector<int> depths;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, m, start;
+	cin >> n >> m >> start;
+
+	adj_list.resize(n + 1);
+	visited = vector<bool>(n + 1, false);
+	depths = vector<int>(n + 1, -1);
+	for (int i = 1; i <= m; i++) {
+		int s, e;
+		cin >> s >> e;
+		adj_list[s].push_back(e);
+		adj_list[e].push_back(s);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(adj_list[i].begin(), adj_list[i].end());
+	}
+
+
+	DFS(start, 0);
+	for (int i = 1; i <= n; i++) {
+		cout << depths[i] << "\n";
+	}
+}
+
+void DFS(int now, int depth) {
+	if (visited[now] == true) {
+		return;
+	}
+	visited[now] = true;
+	depths[now] = depth;
+	cnt++;
+
+	for (int i : adj_list[now]) {
+		if (visited[i] == false) {
+			DFS(i,depth+1);
+		}
+	}
+}
